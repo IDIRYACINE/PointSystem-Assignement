8fd61d6e5f891c3ede3ca648ebd36592bf0582f1
IDIRYACINE <idiryacinesp@gmail.com>
2022-01-07 18:57:04 +0100

    done

--- Src/Main.java
+++ Src/Main.java
@@ -3,9 +3,14 @@ package Src;
 public class Main {
     public static void  main(String args[]){
         PointGym pointGym = new PointGym();
+        // JudgesCOunt , GymnastesCount , EnginesCOunt
+        // Teams are randomly generated and registred
         pointGym.SetUpSystem(12, 20, 3);
         pointGym.StartFirstDayCompetition();
         pointGym.StartSecondDayCompetition();
+        /* CompetitionResult Cant be anounced seperatly must anounce both after the secondDay ends
+            The exercise didn't specify this point ,as it was ambigous on a few more things
+        */
         pointGym.AnnounceCompetitionResults();
     }
 }
